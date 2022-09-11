a99dae74f2cc2ae1b7e985dc2c1cb9d5b910442f
Rintaro ishiguro <rintaroishiguro@Rintaros-MacBook-Air.local>
2022-09-11 10:00:19 +0900

    Added index.html

---
+++ practice.js
@@ -0,0 +1,21 @@
+document.write(23 + 97 + "<br>");
+
+document.write(5 + 8 * 38 / 388 % 8 - 10 + "<br>");
+
+let solution = (4 + 6 + 9) / 77;
+
+document.write(solution);
+document.write("<br>");
+
+let a = 10;
+document.write(a);
+
+let b = 7 * a;
+document.write(b + "<br>");
+
+const max = 57;
+
+let actual = max - 13;
+let percentage = actual / max;
+
+document.write(percentage);
